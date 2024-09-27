0336772eb237dc23823773eee292cc7bc858c510
gaetan <gaetancorso@laposte.net>
2024-09-27 09:08:46 +0200

    add api start (créé grille+ajout btx)

--- Battleship.Models/obj/Debug/net8.0/Battleship.Models.AssemblyInfo.cs
+++ Battleship.Models/obj/Debug/net8.0/Battleship.Models.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Battleship.Models")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("BattleShip.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a30d11660d54f67a68835332220f1bc0382b0d2")]
-[assembly: System.Reflection.AssemblyProductAttribute("Battleship.Models")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Battleship.Models")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46768c2d8a59cdc71f57390de9f05af4ad97840c")]
+[assembly: System.Reflection.AssemblyProductAttribute("BattleShip.Models")]
+[assembly: System.Reflection.AssemblyTitleAttribute("BattleShip.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
